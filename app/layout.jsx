f3e04e635b206e4756d0d1d14e3d07d1c7408361
extrinsic-07 <erastusmatheus3@gmail.com>
2023-06-27 04:34:37 +0200

    v1

---
+++ app/layout.jsx
@@ -0,0 +1,35 @@
+'use client';
+
+import '@styles/global.css';
+
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "Promtopia",
+    description: 'Disover & Share Ai Prompts'
+
+}
+
+const RootLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                <Provider>
+                    <div className="main">
+                        <div className="gradient" />
+
+                    </div>
+
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+
+            </body>
+        </html >
+    )
+}
+
+export default RootLayout;
